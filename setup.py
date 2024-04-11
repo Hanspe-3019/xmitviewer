909bc29099e77f9644a25dc53f8d2fc32c23cf39
Hp i <sql.904@me.com>
2024-04-11 10:56:26 +0200

    Added __main__.py

--- setup.py
+++ setup.py
@@ -1,7 +1,6 @@
 from setuptools import setup
 
 setup(name='xmitviewer',
-        version='0.1',
+        version='0.2',
         packages=['xmitviewer'],
-#       install_requires=['pandas']
         )
